a8ce887f448db9a2450746c5dbfe302b6e60b786
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2022-12-24 21:11:19 -0800

    Update BowlGameFun_streamlit.py

--- BowlGameFun_streamlit.py
+++ BowlGameFun_streamlit.py
@@ -28,9 +28,10 @@ st.header('WELCOME TO THE COLLEGE BOWL PICK-EM 2022-23')
 st.header('Scroll down to see your picks, fun facts about upcoming bowl games and where you stand.')
 
 st.header('We have made it! To another local interest bowl game: The Holiday Bowl!')
-st.image('https://cdn.vox-cdn.com/thumbor/3C6VGijImlfq9l6tMJb3zwOTE1s=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21875845/1190467819.jpg.jpg', width=300)
-st.text('Photo by Photo by Darryl Oumi/Getty Images')
-
+st.image('https://ca-times.brightspotcdn.com/dims4/default/8653424/2147483647/strip/true/crop/5871x3914+265+0/resize/2400x1600!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F25%2Fe1%2Fbc41f46a4caea1f0bd676ee23eb5%2Fsd-photos-1staff-892853-sd-me-holiday-bowl-101.JPG', width=300)
+st.text('Photo from The San Diego Union-Tribune')
+st.subheader('AHHHH! Good ol San Diego!')
+st.subheader(' ')
 st.subheader('Ok, boy does the bowl have some West Coast history! So, first it started in 1978 to give the WAC and automatic bowl bid after\
               the Fiesta Bowl pulled its association with the WAC when Arizona and Arizona State bolted for the Pac8 - to make the Pac10.')
 st.subheader(' ')
@@ -38,7 +39,7 @@ st.subheader('While there have been many games of note for the Pac10/12, because
               the most controversial game was 1984...UGH. I will let you read the write up on wikipedia for the full story, but, in short, the game\
               was relatively close the whole game. Ended 24-17. WOW sounds great, right? This was the year an undefeated BYU team BARELY beat a\
               6-5 Michigan team to "secure" the National Championship. No matter how bad we feel the National Champion is decided now, it is\
-              decidedly better than it was. (e_e)!\')
+              decidedly better than it was. (e_e)!')
 st.subheader(' ')
 st.subheader('BYU is the school that has visited the most, but Oregon has gone 4 times and has a winning record. Let us hope they keep it up!')
               
