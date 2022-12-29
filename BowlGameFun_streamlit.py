882f9666b356c0eedd480b3f72ef9e40ec365627
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2022-12-29 14:12:23 -0800

    Update BowlGameFun_streamlit.py

--- BowlGameFun_streamlit.py
+++ BowlGameFun_streamlit.py
@@ -27,21 +27,19 @@ st.image('https://media.wired.com/photos/5a3af51805f16a0c504434ee/master/pass/fo
 st.header('WELCOME TO THE COLLEGE BOWL PICK-EM 2022-23')
 st.header('Scroll down to see your picks, fun facts about upcoming bowl games and where you stand.')
 
-st.header('We have made it! To another local interest bowl game: The Holiday Bowl!')
+st.header('Orange Bowl and Sun Bowl')
 st.image('https://ca-times.brightspotcdn.com/dims4/default/8653424/2147483647/strip/true/crop/5871x3914+265+0/resize/2400x1600!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F25%2Fe1%2Fbc41f46a4caea1f0bd676ee23eb5%2Fsd-photos-1staff-892853-sd-me-holiday-bowl-101.JPG', width=300)
 st.text('Photo from The San Diego Union-Tribune')
-st.subheader('AHHHH! Good ol San Diego!')
+st.subheader('Orange Bowl is completely ORANGE (Tennessee and Clemson) - wow, take things literally why not?!?!?')
+st.subheader('Sun Bowl is played in "the pass"! Good ol El Paso! What a great place! Not a joke, not sarcastic - I was born there!')
 st.subheader(' ')
-st.subheader('Ok, boy does the bowl have some West Coast history! So, first it started in 1978 to give the WAC and automatic bowl bid after\
-              the Fiesta Bowl pulled its association with the WAC when Arizona and Arizona State bolted for the Pac8 - to make the Pac10.')
+st.subheader('Orange Bowl: Started in 1935. Only a few times has a non-ranked team played in the bowl game.\
+             Only in 1935, 1936, 1938, and 1944, were both teams not ranked. The first year there was an MVP (1965) it was Joe Namath. ')
 st.subheader(' ')
-st.subheader('While there have been many games of note for the Pac10/12, because ironically it then became a tie-in for the Pac10/12 since 1998,\
-              the most controversial game was 1984...UGH. I will let you read the write up on wikipedia for the full story, but, in short, the game\
-              was relatively close the whole game. Ended 24-17. WOW sounds great, right? This was the year an undefeated BYU team BARELY beat a\
-              6-5 Michigan team to "secure" the National Championship. No matter how bad we feel the National Champion is decided now, it is\
-              decidedly better than it was. (e_e)!')
-st.subheader(' ')
-st.subheader('BYU is the school that has visited the most, but Oregon has gone 4 times and has a winning record. Let us hope they keep it up!')
+st.subheader('SUN BOWL!!!!! So we know it is in a great city (El Paso), which is a mile high - take that Denver!\
+             Started in 1935! Two old games today! Oh this is fun: the 1940 game was between the Arizona State Teachers College\
+             at Tempe and the Catholic University Cardinals - the game ended 0-0! Fun Pac12 fact - this is a Pac10/12 tie-in and \
+             only Colorado and Cal have not appeared in the Sun Bowl. Pac12 is 19-12-1.')
 st.subheader(' ')
 st.subheader(' ')
 
